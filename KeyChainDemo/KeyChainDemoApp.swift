b2c68b3d93325a00f9d6d9aeec7076f1d1bd7ecb
Alam Alonzo <alonzo.alam@gmail.com>
2024-12-10 13:11:37 -0400

    Initial Commit

---
+++ KeyChainDemo/KeyChainDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainDemoApp.swift
+//  KeyChainDemo
+//
+//  Created by Alam Alonzo on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyChainDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
